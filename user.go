b97a8fb9e5b7e369b8502a3eecab0263794c7082
Kogmore <1009520441@qq.com>
2022-07-30 16:02:54 +0800

    五、新增功能 当前用户查询所有在线用户信息

--- user.go
+++ user.go
@@ -53,10 +53,29 @@ func (user *User) Offline() {
 	user.Server.BroadCast(user, "已下线!!!")
 }
 
+//SendMsg 给当前User对应的客户端发送消息
+func (user *User) SendMsg(msg string) {
+	_, err := user.Conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Println("user SendMsg error:", err)
+		return
+	}
+}
+
 //DoMessage 用户处理消息的业务
 func (user *User) DoMessage(msg string) {
-	//将得到的消息进行广播
-	user.Server.BroadCast(user, msg)
+	//查询当前在线用户都有哪些
+	if msg == "who" {
+		user.Server.mapLock.Lock()
+		for _, v := range user.Server.OnlineMap {
+			onlineMsg := "[" + v.Addr + "]" + v.Name + ":在线...\n"
+			user.SendMsg(onlineMsg)
+		}
+		user.Server.mapLock.Unlock()
+	} else {
+		//将得到的消息进行广播
+		user.Server.BroadCast(user, msg)
+	}
 }
 
 //ListenMessage 监听当前User channel的方法，一旦有消息，就直接发送给对端 客户端
@@ -65,7 +84,7 @@ func (user *User) ListenMessage() {
 		msg := <-user.C
 		_, err := user.Conn.Write([]byte(msg + "\n"))
 		if err != nil {
-			fmt.Println("user channel error", err)
+			fmt.Println("user channel error:", err)
 			return
 		}
 	}
